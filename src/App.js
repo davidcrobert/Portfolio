0e893ef1a6c8683e6d5f82cbefcdcf92bce962f1
davidcrobert <david.connor.r@gmail.com>
2024-07-01 21:44:15 -0400

    Added AR, Virtual, and Web category pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutMe from './pages/AboutMe';
 import InstallationPage from './pages/InstallationPage';
+import VirtualEnvironmentsPage from './pages/VirtualEnvironmentsPage';
+import AugmentedRealityPage from './pages/AugmentedRealityPage';
+import WebPage from './pages/WebPage';
 
 function App() {
   return (
@@ -12,7 +15,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutMe />} />
           <Route path="/installation" element={<InstallationPage />} />
-          {/* Add more routes here as you create more components */}
+          <Route path="/virtual-environments" element={<VirtualEnvironmentsPage />} />
+          <Route path="/augmented-reality" element={<AugmentedRealityPage />} />
+          <Route path="/web" element={<WebPage />} />
         </Routes>
       </div>
     </Router>
