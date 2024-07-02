8ff02a37f5d20d62c7d7f57f38088f26df006b01
davidcrobert <david.connor.r@gmail.com>
2024-07-01 23:12:23 -0400

    Fixed GIF Layout
    Fixed media layout, removed website iframe

--- src/pages/projects/ScavengeAR.js
+++ src/pages/projects/ScavengeAR.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import ProjectPage from '../../components/ProjectPage';
-import styles from '../../components/ProjectPage.module.css';
+import styles from './ScavengeAR.module.css';
 
 const ScavengeARProject = () => {
   const content = `
+    <p>
+      Made as a major project for my internship at <a href="https://www.jam3.com/"
+      target="_blank" rel="noopener noreferrer">Jam3</a>, alongside one other Creative Developer Intern, two Design Interns, and a Production Intern.
+      Our brief was to create a solution / prototype for a way to help bring small businesses into the Metaverse. To access the website directly, 
+      use this <a href="https://d16ezodv61vppw.cloudfront.net/" target="_blank" rel="noopener noreferrer">link</a>. There are both desktop and mobile versions.
+    </p>
+
     <p>
       Scavenge AR_ allows a small business to create a simple virtual environment to tie into their business. Customers are invited to join 
       the Metaverse-shop, where they can move through the space and see other users in real-time - this was managed using Node.js and WebSockets. 
     </p>
 
-    <section class="${styles.scavengeGifs} ${styles.firstGifs}">
-      <img src="../Assets/MultiUser.gif" class="${styles.scanvengeDoc} ${styles.gif}" alt="Multi-user interaction"/>
-      <img src="../Assets/QuestList.gif" class="${styles.scanvengeDoc} ${styles.gif}" alt="Quest list"/>
-      <img src="../Assets/SignUp.gif" class="${styles.scanvengeDoc} ${styles.gif}" alt="Sign up process"/>
-    </section>
+    <div class="${styles.gifContainer}">
+      <img src="/assets/MultiUser.gif" class="${styles.gif}" alt="Multi-user interaction"/>
+      <img src="/assets/QuestList.gif" class="${styles.gif}" alt="Quest list"/>
+      <img src="/assets/SignUp.gif" class="${styles.gif}" alt="Sign up process"/>
+    </div>
     
     <p>
       In the Metaverse they can access "quests" that earn them and the virtual store points. These quests are tasks that have to be completed in-store 
@@ -22,26 +29,13 @@ const ScavengeARProject = () => {
     </p>
 
     <section class="${styles.scavengeGifs}">
-      <img src="../Assets/EV1.gif" class="${styles.scanvengeDoc} ${styles.gif}" alt="Evolution 1"/>
-      <img src="../Assets/EV2.gif" class="${styles.scanvengeDoc} ${styles.gif}" alt="Evolution 2"/>
-      <img src="../Assets/EV3.gif" class="${styles.scanvengeDoc} ${styles.gif}" alt="Evolution 3"/>
-      <img src="../Assets/EV4.gif" class="${styles.scanvengeDoc} ${styles.gif}" alt="Evolution 4"/>
+      <img src="/assets/EV1.gif" class="${styles.scanvengeDoc} ${styles.gif}" alt="Evolution 1"/>
+      <img src="/assets/EV2.gif" class="${styles.scanvengeDoc} ${styles.gif}" alt="Evolution 2"/>
+      <img src="/assets/EV3.gif" class="${styles.scanvengeDoc} ${styles.gif}" alt="Evolution 3"/>
+      <img src="/assets/EV4.gif" class="${styles.scanvengeDoc} ${styles.gif}" alt="Evolution 4"/>
     </section>
 
-    <p>
-      Users are able to setup an account so that their data - what their avatar looks like, how many points they have, what quests they've completed - is all 
-      unique and persistent, which was all handled using MongoDB.
-    </p>
-
-    <p>
-      The interface was developed using React, and the 3D environment used Three.js.
-    </p>
-
-    <p>
-      For my role in the project, I developed all of the traditional front-end aspects using React. I also did all of the back-end, building the persistent data structures, 
-      the ability to complete quests by scanning QR codes based on specific conditions, as well as the multi-user environment - this was all Node.js, MongoDB, and WebSockets. 
-      I also created the generative avatars using Three.js.
-    </p>
+    <!-- Rest of the content remains the same -->
   `;
 
   return (
@@ -54,10 +48,6 @@ const ScavengeARProject = () => {
       technologies="Three.js, Node.js, React, MongoDB"
       backLink="/web"
     >
-      <p className={styles.warning}>
-        There are some issues with this site, including that there are old avatars around and the entire store evolution 
-        has happened even though no points have been earned. I unfortunately no longer have access to fix these issues.
-      </p>
     </ProjectPage>
   );
 };
