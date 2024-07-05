e58499c2050efc8d88b634567bb4f307f3639328
davidcrobert <david.connor.r@gmail.com>
2024-07-05 13:58:42 -0400

    Updated Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,90 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../styles/Home.module.css';
+import styled from 'styled-components';
 import Sketch from '../components/Sketch';
 import Header from '../components/Header';
 
+const IndexContainer = styled.div`
+  background-color: #f9f9f9;
+  color: black;
+  overscroll-behavior: contain;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  font-family: 'Times New Roman', Times, serif;
+  overflow: hidden;
+`;
+
+const Nav = styled.nav`
+  margin: 0;
+  margin-top: -35px;
+  width: 100%;
+  flex-grow: 1;
+  display: flex;
+  flex-wrap: wrap;
+  row-gap: 25px;
+  align-content: center;
+  justify-content: space-around;
+  text-align: center;
+`;
+
+const NavLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+  font-size: 20px;
+  text-transform: uppercase;
+  transition: transform 0.2s linear;
+  margin: 15px;
+
+  &:hover {
+    cursor: help;
+    transform: rotateX(50deg);
+  }
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 15px;
+  margin-bottom: -5px;
+`;
+
+const FooterLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+  font-size: 20px;
+  text-transform: uppercase;
+  transition: transform 0.2s linear;
+
+  &:hover {
+    cursor: help;
+  }
+`;
+
+const ContactLink = styled.a`
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  text-transform: lowercase;
+  padding-bottom: 20px;
+  color: black;
+  text-decoration: none;
+  font-size: 20px;
+  transition: transform 0.2s linear;
+
+  &:hover {
+    cursor: help;
+    transform: skew(-20deg);
+  }
+`;
+
+const LeftSkew = styled(FooterLink)`
+  &:hover {
+    transform: skew(20deg);
+  }
+`;
+
 function Home() {
   return (
-    <div className={styles.index}>
+    <IndexContainer>
       <Sketch />
       <Header 
         title="David Robert"
@@ -14,22 +92,21 @@ function Home() {
         subtitle2="& Arts Worker"
       />
       
-      <nav className={styles.nav}>
-        <Link to="/installation" className={`${styles.navLink} ${styles.skewBack}`}>↳ Installation</Link>
-        <Link to="/virtual-environments" className={`${styles.navLink} ${styles.skewBack}`}>↳ Virtual Environments</Link>
-        <Link to="/web" className={`${styles.navLink} ${styles.skewBack}`}>↳ Web</Link>
-      </nav>
+      <Nav>
+        <NavLink to="/installation">↳ Installation</NavLink>
+        <NavLink to="/virtual-environments">↳ Virtual Environments</NavLink>
+        <NavLink to="/web">↳ Web</NavLink>
+      </Nav>
 
-      <footer className={styles.footer}>
-        <Link to="/about" className={`${styles.footerLink} ${styles.skewLeft}`}>About Me</Link>
-        <a 
-          className={`${styles.footerLink} ${styles.skewRight} ${styles.contact}`} 
+      <Footer>
+        <LeftSkew to="/about">About Me</LeftSkew>
+        <ContactLink 
           href="mailto:david.connor.r@gmail.com"
         >
           david.connor.r[at]gmail.com
-        </a>
-      </footer>
-    </div>
+        </ContactLink>
+      </Footer>
+    </IndexContainer>
   );
 }
 
