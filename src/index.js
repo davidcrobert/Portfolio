9bfa941620b16dac6a896514fb21b129e232d752
davidcrobert <david.connor.r@gmail.com>
2024-07-04 22:10:12 -0400

    Updated styling
    A bunch

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './styles/scrollbar.css';
+import './styles/mobile.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
